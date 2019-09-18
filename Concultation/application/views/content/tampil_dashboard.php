466c766b9f182e98ab24e4f416e2217133077127
nazirulfauzi <ahmadfauzi@nazirulfauzi.com>
2019-09-19 02:04:58 +0700

    ConcultationDinas

---
+++ Concultation/application/views/content/tampil_dashboard.php
@@ -0,0 +1,15 @@
+<div class="panel">
+								<div class="panel-heading">
+									<h3 class="panel-title">Dashboard</h3>
+									<div class="right">
+										<button type="button" class="btn-toggle-collapse"><i class="lnr lnr-chevron-up"></i></button>
+										<button type="button" class="btn-remove"><i class="lnr lnr-cross"></i></button>
+									</div>
+								</div>
+								<div class="panel-body no-padding bg-primary text-center">
+									<div class="padding-top-30 padding-bottom-30">
+										<i class="fa fa-thumbs-o-up fa-5x"></i>
+										<h3>Aplication Concultation</h3>
+									</div>
+								</div>
+							</div>
